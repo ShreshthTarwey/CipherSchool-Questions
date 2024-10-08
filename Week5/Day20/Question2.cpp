8e2fb16f6beb9dfd7d5c8048cb1ef33699c2a552
Shreshth Tarwey <shreshthtarwey3010@gmail.com>
2024-10-08 20:33:04 +0530

    Week6

---
+++ Week5/Day20/Question2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int ind1 = 0;
+int ind2 = 0;
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int mn = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
